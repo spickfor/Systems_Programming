a08d29cdb6440e5a52fcf5a685de69d147542808
spickfor <spickfor@nd.edu>
2023-02-13 14:39:32 -0500

    removed extra files

--- homework02/measure-latency.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-# TODO: Run latency measuring command once for each command line argument 
-for host in $@; do
-    echo "Measuring latency for $host"
-    ping -c 1 $host
-    echo
-done
