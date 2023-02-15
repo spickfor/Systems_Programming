330b5dc3695a225342873dd275ed39818fd58c64
spickfor <spickfor@nd.edu>
2023-02-15 13:47:18 -0500

    not finished with 4-6

---
+++ exam01/filters.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+q1_answer() {
+    # TODO: List only the last four first names in sorted order
+    curl -sL https://yld.me/raw/g4gJ | cut -d , -f 2 | sort | tail -n 4
+}
+
+q2_answer() {
+    # TODO: Count how many netids do not have any digits
+   curl -sL https://yld.me/raw/g4gJ | cut -d , -f 1 | grep ^[a-zA-Z]*$ | wc -l
+}
+
+q3_answer() {
+    # TODO: List the last names that contain two consecutive vowels in sorted order
+    curl -sL https://yld.me/raw/g4gJ | cut -d , -f 3 | grep -E '.*[aeiou]{2}.*' | sort
+}
+
+q4_answer() {
+    # TODO: List all the phone numbers that contain two of the same even number
+    # and ends with that number in sorted order
+    curl -sL https://yld.me/raw/g4gJ | cut -d , -f 4 | grep -E '.*([2468])\1.*' | sorts
+}
+
+q5_answer() {
+    # TODO: List all the last names that fully appear in the corresponding
+    # netids in sorted order
+    curl -sL https://yld.me/raw/g4gJ
+}
+
+q6_answer() {
+    # TODO: List in sorted order all the netids of people whose phone number
+    # contains at least one duplicate digit in the last four numbers
+    curl -sL https://yld.me/raw/g4gJ
+}
