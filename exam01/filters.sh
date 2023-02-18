54061ab2bb46aacf5b79089138395fcc64d6a9ba
spickfor <spickfor@nd.edu>
2023-02-18 11:40:49 -0500

    finished filters.sh - all 6

--- exam01/filters.sh
+++ exam01/filters.sh
@@ -18,17 +18,17 @@ q3_answer() {
 q4_answer() {
     # TODO: List all the phone numbers that contain two of the same even number
     # and ends with that number in sorted order
-    curl -sL https://yld.me/raw/g4gJ | cut -d , -f 4 | grep -E '.*([2468])\1.*' | sorts
+    curl -sL https://yld.me/raw/g4gJ | cut -d , -f 4 | grep -E '.*([2468]).*\1$' | sort
 }
 
 q5_answer() {
     # TODO: List all the last names that fully appear in the corresponding
     # netids in sorted order
-    curl -sL https://yld.me/raw/g4gJ
+    curl -sL https://yld.me/raw/g4gJ | cut -d ',' -f 1,3 | sed 's/[0-9]//g' | sed 's/^.//' | tr A-Z a-z | awk -F ',' '{ if ($1 == $2) print $2 }' | sort | sed 's/^./\u&/'
 }
 
 q6_answer() {
     # TODO: List in sorted order all the netids of people whose phone number
     # contains at least one duplicate digit in the last four numbers
-    curl -sL https://yld.me/raw/g4gJ
+    curl -sL https://yld.me/raw/g4gJ | cut -d ',' -f 1,4 | grep -E ',.*-.*-.*([0-9]).*\1' | cut -d ',' -f 1 | sort
 }
