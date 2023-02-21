cdf936ee78f352fef4b899ed8b07d9d792b1d71d
spickfor <spickfor@nd.edu>
2023-02-20 20:14:34 -0500

    fixed exists.py basedon Bui instructions

--- reading05/exists.py
+++ reading05/exists.py
@@ -7,10 +7,14 @@ import os
 if (sys.argv[0] == None):
     print("Usage: exists.py file0...")
     
-
+exit =0
 # Check each command line argument
 for arg in sys.argv[1:]:
     if os.path.exists(arg):
         print("%s exists!" %arg)
+        
     else:
-        sys.exit("%s doesn't exist"%arg)
+        print("%s doesn't exist"%arg)
+        exit = 1
+
+sys.exit(exit)
